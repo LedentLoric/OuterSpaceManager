f3fdbf8dea8aaf7893da47a9a3d63dd1df4bd0f2
lledent@formation-cci.fr <lledent@formation-cci.fr>
2018-01-23 08:49:37 +0100

    Rename files

--- app/src/main/java/com/example/lledent/outerspacemanager/SignInActivity.java
+++ app/src/main/java/com/example/lledent/outerspacemanager/SignInActivity.java
@@ -1,72 +1,39 @@
 package com.example.lledent.outerspacemanager;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-/**
- * Created by lledent on 16/01/2018.
- */
-
-public class SignInActivity extends Activity {
+public class SignInActivity extends AppCompatActivity {
     private EditText loginInput;
-    private EditText emailInput;
     private EditText passwordInput;
     private Button sendButton;
+    private Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_activity);
+
         loginInput = (EditText) findViewById(R.id.signinLoginInputID);
-        emailInput = (EditText) findViewById(R.id.signinEmailInputID);
         passwordInput = (EditText) findViewById(R.id.signinPasswordInputID);
         sendButton = (Button) findViewById(R.id.signinSendButtonID);
+        signInButton = (Button) findViewById(R.id.signinSignInButtonID);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                User user = new User();
-                user.setUsername(loginInput.getText().toString());
-                user.setEmail(emailInput.getText().toString());
-                user.setPassword((passwordInput.getText().toString()));
 
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://outer-space-manager.herokuapp.com")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
-                OuterSpaceManagerService service = retrofit.create(OuterSpaceManagerService.class);
-                Call<ConnectionResponse> request= service.createUser(user);
-
-                request.enqueue(new Callback<ConnectionResponse> (){
-                    @Override
-                    public void onResponse(Call<ConnectionResponse> call, Response<ConnectionResponse> response) {
-                        if (response.code() == 200) {
-                            Toast.makeText(getApplicationContext(),"Le compte a été créé.",Toast.LENGTH_SHORT).show();
-
-                        } else {
-                            Toast.makeText(getApplicationContext(),"Erreur",Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<ConnectionResponse> call, Throwable t) {
-
-                        
-                    }
-                });
+            }
+        });
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInActivity = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(signInActivity);
             }
         });
     }
--- app/src/main/java/com/example/lledent/outerspacemanager/SignUpActivity.java
+++ app/src/main/java/com/example/lledent/outerspacemanager/SignUpActivity.java
@@ -1,39 +1,69 @@
 package com.example.lledent.outerspacemanager;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by lledent on 16/01/2018.
+ */
+
+public class SignUpActivity extends Activity {
     private EditText loginInput;
+    private EditText emailInput;
     private EditText passwordInput;
     private Button sendButton;
-    private Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
-
         loginInput = (EditText) findViewById(R.id.signupLoginInputID);
+        emailInput = (EditText) findViewById(R.id.signupEmailInputID);
         passwordInput = (EditText) findViewById(R.id.signupPasswordInputID);
         sendButton = (Button) findViewById(R.id.signupSendButtonID);
-        signInButton = (Button) findViewById(R.id.signupSignInButtonID);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                User user = new User();
+                user.setUsername(loginInput.getText().toString());
+                user.setEmail(emailInput.getText().toString());
+                user.setPassword((passwordInput.getText().toString()));
 
-            }
-        });
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent signInActivity = new Intent(getApplicationContext(), SignInActivity.class);
-                startActivity(signInActivity);
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("https://outer-space-manager.herokuapp.com")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                OuterSpaceManagerService service = retrofit.create(OuterSpaceManagerService.class);
+                Call<ConnectionResponse> request= service.createUser(user);
+
+                request.enqueue(new Callback<ConnectionResponse> (){
+                    @Override
+                    public void onResponse(Call<ConnectionResponse> call, Response<ConnectionResponse> response) {
+                        if (response.code() == 200) {
+                            Toast.makeText(getApplicationContext(),"Le compte a été créé.",Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(getApplicationContext(),"Erreur",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ConnectionResponse> call, Throwable t) {
+
+
+                    }
+                });
             }
         });
     }
